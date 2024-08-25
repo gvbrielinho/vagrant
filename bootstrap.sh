569ae17ca1a6d4b559333791f928ac519e524b83
Gabriel Gomez <gabriel.gomez@e3ecommerce.com.ar>
2024-08-25 13:39:37 -0300

    changes: updates in apache

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,6 @@ apt-get update
 apt-get install -y apache2
 cp /vagrant/000-default.conf /etc/apache2/sites-available/000-default.conf
 cp -r /vagrant/app/* /var/www/html/
+echo "AddDefaultCharset UTF-8" >> /etc/apache2/conf-available/charset.conf
+a2enconf charset
 systemctl restart apache2
